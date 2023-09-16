0c24940ce14701194e8e677eecb6dc2aa7d5cfce
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-09-16 15:48:57 +0300

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QR_MENU.Models;
+using System.Diagnostics;
+
+namespace QR_MENU.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
